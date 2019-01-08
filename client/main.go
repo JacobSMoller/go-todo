e2a55e90039c68014fa5eb808c434bd609fd98c2
JacobSMoller <scherffenberg91@gmail.com>
2019-01-08 15:02:04 +0100

    Add gettodo

--- client/main.go
+++ client/main.go
@@ -23,16 +23,22 @@ func main() {
 	c := pb.NewTodoServiceClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	item := &pb.Todo{
-		Title:       "Foo task",
-		Description: "A task created from my client",
-	}
-	req := &pb.CreateTodoRequest{Item: item}
-	r, err := c.CreateTodo(ctx, req)
-	if err != nil {
-		log.Fatalf("Could not create item %v", err)
-	}
-	log.Printf("Created todo with id: %d", r.Id)
+	// future_time, err := time.Parse(time.RFC3339, "2020-01-10T10:44:00+00:00")
+	// if err != nil {
+	// 	log.Fatalf("Failed to parse future time %v", err)
+	// }
+	// proto_future_time, err := ptypes.TimestampProto(future_time)
+	// item := &pb.Todo{
+	// 	Title:       "Foo task",
+	// 	Description: "A task created from my client",
+	// 	CreatedAt:   proto_future_time,
+	// }
+	// req := &pb.CreateTodoRequest{Item: item}
+	// r, err := c.CreateTodo(ctx, req)
+	// if err != nil {
+	// 	log.Fatalf("Could not create item %v", err)
+	// }
+	// log.Printf("Created todo with id: %d", r.Id)
 
 	// del_req := &pb.DeleteTodoRequest{Id: 4}
 	// del_res, err := c.DeleteTodo(ctx, del_req)
@@ -52,4 +58,6 @@ func main() {
 	// 	log.Fatalf("Could not create item %v", err)
 	// }
 	// log.Printf("Created owner with id: %d", r.Id)
+
+	req := &pb.GetTodoRequest{Id: 1}
 }
--- service/todo/todo.go
+++ service/todo/todo.go
@@ -18,25 +18,35 @@ type Service struct {
 
 // CreateTodo creates a todo given a description
 func (s Service) CreateTodo(ctx context.Context, req *todo.CreateTodoRequest) (*todo.CreateTodoResponse, error) {
-	err := s.DB.Create(req.Item)
-	if err != nil {
-		return nil, grpc.Errorf(codes.Internal, "Could not insert item into the database: %v", err)
+	result := s.DB.Create(req.Item)
+	if result.Error != nil {
+		return nil, grpc.Errorf(codes.Internal, "Could not insert item into the database: %v", result.Error)
 	}
 	return &todo.CreateTodoResponse{Id: req.Item.Id}, nil
 }
 
 func (s Service) DeleteTodo(ctx context.Context, req *todo.DeleteTodoRequest) (*todo.DeleteTodoResponse, error) {
-	err := s.DB.Create(&todo.Todo{Id: req.Id})
-	if err != nil {
-		return nil, grpc.Errorf(codes.Internal, "Could not delete the item: %v", err)
+	result := s.DB.Create(&todo.Todo{Id: req.Id})
+	if result.Error != nil {
+		return nil, grpc.Errorf(codes.Internal, "Could not delete the item: %v", result.Error)
 	}
 	return &todo.DeleteTodoResponse{}, nil
 }
 
 func (s Service) CreateOwner(ctx context.Context, req *todo.CreateOwnerRequest) (*todo.CreateOwnerResponse, error) {
-	err := s.DB.Create(req.Owner)
-	if err != nil {
-		return nil, grpc.Errorf(codes.Internal, "Could not create owner: %v", err)
+	result := s.DB.Create(req.Owner)
+	if result.Error != nil {
+		return nil, grpc.Errorf(codes.Internal, "Could not create owner: %v", result.Error)
 	}
 	return &todo.CreateOwnerResponse{Id: req.Owner.Id}, nil
+}
+
+// GetTodo retrieves a todo item from its ID
+func (s Service) GetTodo(ctx context.Context, req *todo.GetTodoRequest) (*todo.GetTodoResponse, error) {
+	var item todo.Todo
+	result := s.DB.First(&item, req.Id)
+	if result.Error != nil {
+		return nil, grpc.Errorf(codes.NotFound, "Could not retrieve item from the database: %s", result.Error)
+	}
+	return &todo.GetTodoResponse{Item: &item}, nil
 }
